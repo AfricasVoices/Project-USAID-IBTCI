686c9172ac34bec9ba51cd65dac1928a7ea9904d
Alexander Simpson <as2388@cam.ac.uk>
2020-09-15 11:20:10 +0100

    Load SMS code schemes in code_schemes.py

--- configuration/code_schemes.py
+++ configuration/code_schemes.py
@@ -12,4 +12,21 @@ def _open_scheme(filename):
 class CodeSchemes(object):
     FACEBOOK_S01E01 = _open_scheme("facebook_s01e01.json")
 
-    WS_CORRECT_DATASET = None  # TODO Add scheme
+    RQA_S08E01 = _open_scheme("rqa_s08e01.json")
+    RQA_S08E02 = _open_scheme("rqa_s08e02.json")
+    RQA_S08E03 = _open_scheme("rqa_s08e03.json")
+
+    SOMALIA_OPERATOR = _open_scheme("somalia_operator.json")
+
+    AGE = _open_scheme("age.json")
+    GENDER = _open_scheme("gender.json")
+    MOGADISHU_SUB_DISTRICT = _open_scheme("mogadishu_sub_district.json")
+    SOMALIA_DISTRICT = _open_scheme("somalia_district.json")
+    SOMALIA_REGION = _open_scheme("somalia_region.json")
+    SOMALIA_STATE = _open_scheme("somalia_state.json")
+    SOMALIA_ZONE = _open_scheme("somalia_zone.json")
+    RECENTLY_DISPLACED = _open_scheme("recently_displaced.json")
+    LIVELIHOOD = _open_scheme("livelihood.json")
+    HOUSEHOLD_LANGUAGE = _open_scheme("household_language.json")
+
+    WS_CORRECT_DATASET = _open_scheme("ws_correct_dataset.json")
